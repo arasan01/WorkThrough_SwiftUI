af201fe3f2811d605fae9e70c79cc1d2711a9ed9
Arayama Kyo <sinzan062871@gmail.com>
2020-10-19 22:26:11 +0900

    Initial Commit

---
+++ WorkThroughView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkThroughView
+//
+//  Created by あらさん on 2020/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WorkThroughView/WorkThroughViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkThroughViewApp.swift
+//  WorkThroughView
+//
+//  Created by あらさん on 2020/10/19.
+//
+
+import SwiftUI
+
+@main
+struct WorkThroughViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
